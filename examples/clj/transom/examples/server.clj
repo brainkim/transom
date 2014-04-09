eed81a7bca812eb33b692f7cfb69462de6a3bc6c
Brian Kim <briankimpossible@gmail.com>
2014-04-08 21:03:59 -0400

    FUCKING CRUSHING IT

--- examples/clj/transom/examples/server.clj
+++ examples/clj/transom/examples/server.clj
@@ -4,28 +4,42 @@
             [ring.middleware.resource :refer [wrap-resource]]
             [ring.middleware.edn :refer [wrap-edn-params]]
             [org.httpkit.server :refer [run-server with-channel on-close on-receive send!]]
-            [compojure.core :refer [defroutes GET]]
+            [compojure.core :refer [routes GET]]
             [compojure.route :as route]
-            [transom.core :as transom]))
+            [transom.core :as transom]
+            [com.stuartsierra.component :as component])
+  (:gen-class))
 
 (defonce server! (atom nil))
 (defonce channels! (atom #{}))
-(defonce inbox (chan))
+(def killer (chan))
+(def inbox (chan))
 
 (defn web-socket [request]
   (with-channel request c
     (on-close c (fn [status] (swap! channels! disj c)))
     (on-receive c (fn [data] (put! inbox [(read-string data) c])))))
 
-(defroutes router
-  (GET "/textarea" [] web-socket)
-  (route/files "/" {:root "resources"})
-  (route/not-found "¯\\_(ツ)_/¯"))
-
 (defn app
   []
-  (-> router
+  (-> (routes (GET "/textarea" [] web-socket)
+              (route/files "/" {:root "resources"})
+              (route/not-found "¯\\_(ツ)_/¯"))
       wrap-edn-params))
+(defn rand-char
+  []
+  (let [chars (map char (concat (range 48 58) (range 66 92) (range 97 123)))]
+    (rand-nth chars)))
+
+(defn rand-edit
+  [doc]
+  (transom/pack
+    (let [doc-len (count doc)
+          entry (rand-int doc-len)
+          edit-delta (inc (rand-int 5))]
+      [[:= entry]
+       [:+ (apply str (repeatedly edit-delta rand-char))]
+       [:= (- (count doc) entry)]])))
 
 (defn send-all!
   [message]
@@ -34,40 +48,62 @@
       (send! c message))))
 
 (defn stop-server
-  [kill-chan]
+  []
   (when-not (nil? @server!)
     (@server! :timeout 100)
     (reset! server! nil)
     (reset! channels! #{})
-    (put! kill-chan :kill)
+    (put! killer :kill)
     (println "Server stoppped!")))
 
 (defn start-server
   []
-  (let [kill (chan)
-        ids! (async/to-chan (range))
-        version! (async/to-chan (range))
-        document! (atom "yr a big boob") 
+  (let [ids! (async/to-chan (range))
+        document! (atom {:doc "" :version 0})
         inbox* (async/pub inbox #(get-in % [0 :type]))
-        inits (async/sub inbox* :init (chan))
-        edits (async/sub inbox* :edit (chan))]
+        inits  (async/sub inbox* :init (chan))
+        edits  (async/sub inbox* :edit (chan))]
     (go-loop []
       (alt!
-        kill ([_] println "POOPIE!")
-        inits ([[data noob]]
-                (println data)
-                (swap! channels! conj noob)
-                (let [response {:type :init
-                                :id (<! ids!)
-                                :doc @document!}]
-                  (send! noob (pr-str response)))
-                  (recur))
-        edits ([[data chan]]
-                (println data)
-                (send-all! data)
-                (recur))))
-    (reset! server! (run-server (app) {:port 8000}))
-    #(stop-server kill)))
+        killer
+        ([_]
+          (println "You killed me"))
+        inits
+        ([[data sock]]
+          (swap! channels! conj sock)
+          (let [{:keys [version doc]} @document!
+                response {:type :init
+                          :id (<! ids!)
+                          :version version
+                          :doc doc}]
+          (send! sock (pr-str response)))
+          (recur))
+        edits
+        ([[data sock]]
+          (swap! document!
+                 (fn [{:keys [doc version] :as document}]
+                   (if-let [{:keys [edit]} data]
+                     {:doc (transom/patch doc edit)
+                      :version (inc version)}
+                     document)))
+          (send-all! data)
+          (recur))))
+    (reset! server! (run-server (app) {:port 8000}))))
+
+(defn rand-char
+  []
+  (let [chars (map char (concat (range 48 58) (range 66 92) (range 97 123)))]
+    (rand-nth chars)))
+
+(defn rand-edit
+  [doc]
+  (transom/pack
+    (let [doc-len (count doc)
+          entry (rand-int doc-len)
+          edit-delta (inc (rand-int 5))]
+      [[:= entry]
+       [:+ (apply str (repeatedly edit-delta rand-char))]
+       [:= (- (count doc) entry)]])))
 
 (defn -main
   []
--- examples/cljs/transom/examples/textarea.cljs
+++ examples/cljs/transom/examples/textarea.cljs
@@ -1,34 +1,20 @@
 (ns transom.examples.textarea
   (:require-macros [cljs.core.async.macros :refer [go go-loop alt!]])
-  (:require [cljs.core.async :as async :refer [put! chan >! <! alts!]]
+  (:require [cljs.core.async :as async :refer [put! chan >! <! timeout]]
             [cljs.reader :refer [read-string]]
             [om.core :as om :include-macros true]
             [om.dom :as dom :include-macros true]
             [transom.core :as transom]
             [goog.dom :as gdom]
-            [goog.events :as events]
+            [goog.dom.selection :as selection]
+            [goog.events :refer [listen]]
             [goog.net.WebSocket.EventType :refer [OPENED CLOSED ERROR MESSAGE]])
-  (:import goog.net.WebSocket))
+  (:import (goog.net WebSocket)))
 (enable-console-print!)
 
 (defn host
   []
   (.. js/window -location -host)) 
-
-(defn collaborative-textarea
-  [data owner]
-  (letfn [(change [e]
-            (let [message (.. e -target -value)]
-              (om/update! data [:doc] message)))]
-    (reify
-      om/IRender
-      (render [_]
-        (dom/textarea #js {:disabled (boolean (:doc data))
-                           :value (or (:doc data) "")
-                           :autoFocus true
-                           :rows 50
-                           :cols 80
-                           :onChange change})))))
 
 (defn app
   [data owner]
@@ -36,21 +22,22 @@
             (let [message (.. e -target -value)]
               (om/update! data [:doc] message)))]
     (reify
+      om/IWillUpdate
+      (will-update [_ props state]
+        (let [node (.. owner -refs -textarea getDOMNode)]
+          (selection/setStart node 0)
+          (selection/setEnd node 0)))
       om/IRender
       (render [_]
-        (println "data "data)
-        (println "should be nil" (:doc data))
-        (println "should be false"(boolean nil))
-        (println "should also be false" (boolean (:doc data)))
-        (println "should be true" (not (:doc data)))
         (dom/div nil
-          (dom/textarea #js {:disabled (not (:doc data))
-                             :value (or (:doc data) "")
+          (dom/textarea #js {:disabled (nil? (:version data))
+                             :value (:doc data)
+                             :ref "textarea"
                              :autoFocus true
                              :rows 50
                              :cols 80
                              :onChange change}
-          (dom/div nil (str "Data: "(pr-str data)))))))))
+          (dom/div nil (str "Data: " (pr-str data)))))))))
 
 (defn diff
   [a b]
@@ -59,8 +46,7 @@
     (letfn
       [(common-prefix [a b]
          (loop [i 0]
-           (if (and (< i a-len) (< i b-len)
-                    (= (.charAt a i) (.charAt b i)))
+           (if (and (< i a-len) (< i b-len) (= (.charAt a i) (.charAt b i)))
              (recur (inc i))
              i)))
        (common-suffix [a b]
@@ -79,88 +65,122 @@
                          [:+ (subs b pre (- b-len suf))]
                          [:= suf]]))))))
 
-(defn conductor
-  [in in* out out*]
-  (let [kill (chan)]
-    (go-loop [pending nil buffer nil]
-      (alt!
-        in
-        ([edit]
-         (if (nil? pending)
-           (>! in* edit)
-           (let [[pending' edit'] (transom/transform pending edit)]
-             (if (nil? buffer)
-               (do
-                 (>! in* edit')
-                 (recur pending' nil))
-               (let [[buffer' edit''] (transom/transform buffer edit')]
-                 (>! in* edit'')
-                 (recur pending' buffer'))))))
-        out
-        ([edit]
-         (>! out* edit)
-         (if (nil? pending)
-           (recur edit nil)
-           (let [buffer' (if (nil? buffer)
-                          edit
-                          (transom/compose buffer edit))]
-             (recur pending buffer'))))
-        kill
-        ([kill]
-         (println "Goodbye. I love you baby."))))
-    kill))
+(defn rand-char
+  []
+  (let [chars (map char (concat (range 48 58) (range 66 92) (range 97 123)))]
+    (rand-nth chars)))
+
+(defn rand-edit
+  [doc]
+  (transom/pack
+    (let [doc-len (count doc)
+          entry (rand-int doc-len)]
+      [[:= entry]
+       [:+ "pizza"]
+       [:= (- doc-len entry)]])))
+
+(defn mocksocket
+  [in out ws]
+  (go-loop []
+    (println (<! out))
+    (recur))
+  #_(go-loop [doc "" version 0]
+    (<! (timeout 1500))
+    (let [edit (rand-edit doc)
+          doc (transom/patch doc edit)]
+      (>! in {:type :edit
+              :id :FUCK_YOU
+              :edit edit
+              :version version})
+      (recur doc (inc version)))))
 
 (defn websocket
-  [inbox outbox]
-  (let [ws (WebSocket. false)]
-    (events/listen ws OPENED #(.send ws (pr-str {:type :init})))
-    (events/listen ws CLOSED #(async/close! inbox))
-    (events/listen ws ERROR #(do (println "ERROR" %) (async/close! inbox)))
-    (events/listen ws MESSAGE (fn [ev]
-                                (let [message (.-message ev)
-                                      message (read-string message)]
-                                  (put! inbox message))))
-    (.open ws (str "ws://" (host) "/textarea"))
-    (go-loop []
-      (when-let [m (<! outbox)]
-        (.send ws (pr-str m))
+  [in out ws]
+  (listen ws OPENED 
+          (fn [_]
+            (.send ws (pr-str {:type :init}))))
+  (listen ws CLOSED
+          (fn [_]
+            (async/close! in)))
+  (listen ws ERROR
+          (fn [ev]
+            (println "ERROR" ev)
+            (async/close! in)))
+  (listen ws MESSAGE
+          (fn [ev]
+            (let [message (.-message ev)
+                  message (read-string message)]
+              (put! in message))))
+  (.open ws (str "ws://" (host) "/textarea"))
+  (go-loop []
+    (when-let [m (<! out)]
+      (.send ws (pr-str m))
+      (recur))
+    (.close ws)))
+
+(defn controller
+  [in out out* app-state! stage!]
+  (go-loop []
+    (alt!
+      in
+      ([message]
+        (println @stage!)
+        (if (not= (:id message) (:id @app-state!))
+          (let [{:keys [pending buffer]} @stage!
+                {:keys [version edit]} message]
+            (if (nil? pending)
+              (swap! app-state!
+                     #(merge {:doc (transom/patch (:doc %) edit)
+                              :version version}))
+              (let [[pending' edit'] (transom/transform pending edit)]
+                (if (nil? buffer)
+                  (do 
+                    (swap! stage! assoc :pending pending')
+                    (swap! app-state!
+                           #(merge {:doc (transom/patch (:doc %) edit')
+                                    :version version})))
+                  (let [[buffer' edit''] (transom/transform buffer edit')]
+                    (do
+                      (swap! stage! merge {:pending pending' :buffer buffer'})
+                      (swap! app-state!
+                             #(merge {:doc (transom/patch (:doc %) edit'')
+                                      :version version})))))))))
         (recur))
-      (.close ws))))
-(def fake-thing {:foo nil})
-(println "Is this working?" (boolean (:foo fake-thing)))
+      out
+      ([message]
+        (let [{:keys [edit]} message]
+          (swap! stage! (fn [{:keys [pending buffer]}]
+                 (if (nil? pending)
+                   (do
+                     (put! out* edit)
+                     {:pending edit
+                      :buffer nil})
+                   (if (nil? buffer)
+                     {:pending pending
+                      :buffer edit}
+                     {:pending pending
+                      :buffer (transom/compose buffer edit)})))))
+        (recur)))))
 
-
-;; Sync shouldn't deal with any parsing or anything like that right?
-;; This is a dumb name and you should feel bad
 (defn -main
   []
-  (let [app-state! (atom {:doc nil :id nil :version nil})
-        in    (chan)
-        out   (chan)
-        in*   (async/pub in :type)
-        inits (async/sub in* :init (chan))
-        edits (async/sub in* :edit (chan))
-        wut   (async/sub in* nil (chan))]
-    (websocket in out)
-    (let [init
-          (go (when-let [message (<! inits)]
-            (swap! app-state! merge message)))])
-    (go
-      (<! init)
-      (loop
-        (when-let [message (<! edits)]
-          (let [{:keys [edit]} message]
-            )
-          (recur))))
-    (om/root
-      app
-      app-state!
-      {:target (gdom/getElement "app")
-       :tx-listen
-       (fn [{:keys [old-value new-value path]} cursor]
-         (let [{:keys [id last-seen]} @app-state!]
-           (put! out {:id id
-                      :last-seen last-seen
-                      :type :edit
-                      :edit (diff old-value new-value)})))})))
+  (letfn [(patch-doc
+            [app-state edit]
+            (assoc-in app-state :doc (transom/patch (:doc app-state) edit)))]
+    (let [app-state! (atom {:doc "" :id nil :version 0})
+          stage! (atom {:pending nil :buffer nil})
+          in (chan)
+          out (chan) out* (chan)]
+      (mocksocket in out* (WebSocket. false))
+      (controller in out out* app-state! stage!)
+      (om/root app
+        app-state!
+        {:target (gdom/getElement "app")
+         :tx-listen
+         (fn [{:keys [old-value new-value path]} cursor]
+           (let [{:keys [id version]} @app-state!]
+             (put! out {:type :edit
+                        :id id
+                        :path path
+                        :edit (diff old-value new-value)})))}))))
 (-main)
