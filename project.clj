dec13e1a86708286bd0fd3699d75e3dd24d7019c
Brian Kim <briankimpossible@gmail.com>
2014-07-11 00:43:58 -0400

    Bump lang deps

--- project.clj
+++ project.clj
@@ -2,11 +2,12 @@
   :description "Operational Transformations in Clojure/Clojurescript"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-2202"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/clojurescript "0.0-2268"]
                  [com.keminglabs/cljx "0.3.2"]
                  [org.clojure/core.match "0.2.1"]
-                 [org.clojure/core.async "0.1.278.0-76b25b-alpha"]]
+                 [org.clojure/core.async "0.1.278.0-76b25b-alpha"]
+                 [org.clojure/core.typed "0.2.48"]]
   :source-paths ["src/cljx"]
   :test-paths ["target/test-classes"]
   :profiles {:dev
