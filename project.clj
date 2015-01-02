213e2a8ccb28420c5a45c2c0c018af77f11684f0
Brian Kim <briankimpossible@gmail.com>
2015-01-02 04:15:19 -0500

    increment cljx

--- project.clj
+++ project.clj
@@ -3,8 +3,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2268"]
-                 [com.keminglabs/cljx "0.3.2"]]
+                 [org.clojure/clojurescript "0.0-2268"]]
   :source-paths ["src/clj" "src/cljx"]
   :test-paths ["test" "target/test-classes"]
   :profiles {:dev
@@ -20,11 +19,9 @@
                              [org.clojure/core.async "0.1.278.0-76b25b-alpha"] 
                              [bidi "1.10.2"]]
               :plugins [[lein-cljsbuild "1.0.3"]
-                        [com.keminglabs/cljx "0.4.0"]]
-              ;clj
+                        [com.keminglabs/cljx "0.5.0"]]
               :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
               :source-paths ["examples/clj"]
-              :jvm-opts ^:replace ["-server" "-Xms3072m" "-Xmx3072m"]
               :cljsbuild
               {:builds [{:id "textarea"
                          :source-paths ["target/classes"
